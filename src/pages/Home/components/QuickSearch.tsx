30165a90dc487258d79c634fb51cb736b47c84c3
zensayyy <zensayyy@gmail.com>
2022-12-26 14:32:37 +0100

    #11: fixed ZIndex, input stylin

--- src/pages/Home/components/QuickSearch.tsx
+++ src/pages/Home/components/QuickSearch.tsx
@@ -1,10 +1,22 @@
+import { useMemo } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { AutocompleteDropdown } from "react-native-autocomplete-dropdown";
 
 const autocompleteStyles = StyleSheet.create({
   container: {
     borderRadius: 12,
-    backgroundColor: "#171717"
+    backgroundColor: "#171717",
+  },
+  rightButton: {
+  },
+  suggestionsListContainer: {
+    backgroundColor: "#171717",
+    color: "rgb(212,212,216)",
+    border: 0,
+  },
+  suggestionsListText: {
+    color: "rgb(212,212,216)",
+    border: 0,
   }
 });
 
@@ -15,14 +27,32 @@ const data = [
 ];
 
 export default function QuickSearch() {
+
   return (
     <View className="w-full mt-3">
       <Text className="text-gray-100 font-thin">Find your favourite Songs!</Text>
       < View className="relative w-full z-50 mt-2" >
         <AutocompleteDropdown
+          inputContainerStyle={autocompleteStyles.container}
+          suggestionsListContainerStyle={autocompleteStyles.suggestionsListContainer}
+          suggestionsListTextStyle={autocompleteStyles.suggestionsListText}
           key={"dropdown"}
           showChevron={false}
           dataSet={data}
+          renderItem={(item) => (
+            <Text className="text-gray-400">
+              {item.title}
+            </Text>
+          )}
+          ItemSeparatorComponent={<View />}
+          textInputProps={{
+            placeholder: "Search Song",
+            autoCorrect: false,
+            autoCapitalize: 'none',
+            style: {
+              color: "rgb(212,212,216)",
+            },
+          }}
         />
       </View >
     </View >
