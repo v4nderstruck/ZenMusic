ed8efd6b991261fd2dd6990f89b5dd0b9fad8d3e
zensayyy <zensayyy@gmail.com>
2022-12-26 21:29:58 +0100

    #11: auto suggestions

--- src/pages/Home/components/QuickSearch.tsx
+++ src/pages/Home/components/QuickSearch.tsx
@@ -1,7 +1,8 @@
-import { useMemo } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { useMemo, useState } from "react";
+import { Dimensions, StyleSheet, Text, View } from "react-native";
 import { AutocompleteDropdown } from "react-native-autocomplete-dropdown";
 import Icon from 'react-native-vector-icons/Ionicons';
+import SearchSuggestionProvider from "../../../common/Providers/SearchSuggestionProvider";
 
 const autocompleteStyles = StyleSheet.create({
   container: {
@@ -14,7 +15,7 @@ const autocompleteStyles = StyleSheet.create({
   },
   suggestionsListContainer: {
     backgroundColor: "#171717",
-    opacity: 0.97,
+    opacity: 0.99,
     color: "rgb(212,212,216)",
     border: 0,
   },
@@ -28,6 +29,15 @@ const data = [
   { id: "1", title: "test" },
   { id: "2", title: "testa" },
   { id: "3", title: "testb" },
+  { id: "4", title: "test" },
+  { id: "5", title: "testa" },
+  { id: "7", title: "testb" },
+  { id: "8", title: "test" },
+  { id: "99", title: "testa" },
+  { id: "32", title: "testb" },
+  { id: "42", title: "test" },
+  { id: "51", title: "testa" },
+  { id: "72", title: "testb" },
 ];
 
 const selectItemHandler = (item: any) => {
@@ -38,19 +48,38 @@ const submitHandler = ({ nativeEvent }: any) => {
   console.log(nativeEvent.text)
 }
 
-export default function QuickSearch() {
+interface Suggestion {
+  id: string,
+  title: string,
+}
 
+export default function QuickSearch() {
+  const [suggestionList, setSuggestionList] = useState<Suggestion[]>([])
+  const inputChangeHandler = async (input: string) => {
+    const suggestions = await SearchSuggestionProvider.fetch(input);
+    console.log(suggestions)
+    const set = suggestions.map(item => {
+      return {
+        id: item.query,
+        title: item.text
+      } as Suggestion
+    });
+    setSuggestionList(set)
+  }
   return (
     <View className="w-full mt-3">
       <Text className="text-gray-100 font-thin">Find your favourite Songs!</Text>
       < View className="relative w-full z-50 mt-2" >
         <AutocompleteDropdown
+          debounce={600}
+          suggestionsListMaxHeight={Dimensions.get("screen").height * 0.8}
           onSelectItem={selectItemHandler}
           onSubmit={submitHandler}
           inputContainerStyle={autocompleteStyles.container}
           suggestionsListContainerStyle={autocompleteStyles.suggestionsListContainer}
           suggestionsListTextStyle={autocompleteStyles.suggestionsListText}
-          dataSet={data}
+          onChangeText={inputChangeHandler}
+          dataSet={suggestionList}
           renderItem={(item) => (
             <Text className="text-gray-400 p-2">
               {item.title}
